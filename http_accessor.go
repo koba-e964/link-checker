56120f490edff1d39a55283e0cce62e7e1e6a55d
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2024-03-03 10:02:45 +0900

    Add User Agent

--- http_accessor.go
+++ http_accessor.go
@@ -6,7 +6,13 @@ type HttpAccessor = func(url string) (int, error)
 
 // Returns the status code.
 func httpHead(url string) (int, error) {
-	resp, err := http.Head(url)
+	client := http.Client{}
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("User-Agent", "link-checker from https://github.com/koba-e964/link-checker")
+	resp, err := client.Do(req)
 	if err != nil {
 		return 0, err
 	}
